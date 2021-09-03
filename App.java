0a726ccd4f624d877fe6ed738d69305e43eda16d
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-03 20:57:06 +0300

    Add encryption and decryption process

--- App.java
+++ App.java
@@ -3,93 +3,85 @@ import java.io.Console;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String [] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Have fun interacting with our app!");
-        boolean programmeLoop = true;
+    private String text;
+    private int key;
 
-        while(programmeLoop){
-            System.out.println("------------------------------------------------------");
-            System.out.println("Enter one of the options below to proceed");
-            System.out.println("A. Encrypt And Decrypt Text");
-            System.out.println("B. Encrypt And Decrypt File");
-            System.out.println("C. Exit");
-            try{
+    public static void main (String[] args){
 
+        boolean loopRunning = true;
 
-                String choice = myObj.readLine();
-                System.out.println(choice);
+        while(loopRunning){
+            System.out.println("Have fun interacting with the app");
+            System.out.println("Enter either of the options to proceed");
+            System.out.print("1. Encrypt text:");
+            System.out.println("2. Decrypt text:");
+            Scanner newScanner = new Scanner(System.in);
+            int option = newScanner.nextInt();
 
-                if(choice.equals("A")){
+            if (option == 1){
+                System.out.println("Let's encrypt your text");
+                newScanner.nextLine();
+                System.out.println("Enter Text:");
+                String text = newScanner.nextLine();
 
-                    System.out.print("Please enter your text");
-                    String originalMessage = myObj.readLine();
+                System.out.println("Enter decryption key between 1 to 25:");
+                Integer key = Integer.parseInt(newScanner.nextLine());
 
-                    System.out.print("Please enter  Key between 1 and 25");
-                    int shiftKey = Integer.parseInt(myObj.readLine());
+                Encrypt newEncryption = new Encrypt(text, key);
+                System.out.print("Enter either of the options to proceed: ");
 
-                    if(shiftKey>0 && shiftKey<26){
-                        Caesar msgCaesar = new Caesar(originalMessage,shiftKey);
-                        EncryptClass msgEncrypt = new EncryptClass();
-                        Decrypt msgDecrypt =new Decrypt();
-
-                        msgEncrypt.EncryptMessage(msgCaesar.getMessage(), msgCaesar.getShiftKey());
-                        msgDecrypt.decryptMessage(msgEncrypt.getEncryptedMessage(),msgCaesar.getShiftKey() );
-
-                        System.out.println("Input Message :"+ originalMessage);
-                        System.out.println("Encrypted Message :"+msgEncrypt.getEncryptedMessage() );
-
-                        System.out.println("Decrypted Message :"+msgDecrypt.getDecryptedMessage() );
-                    }else{
-                        System.out.println(" Key Must be between 1 and 25");
-
-                    }
-                }else if(choice.equals("B")){
-                    System.out.println("Enter File Path.Example D://data.txt");
-                    String filePath = myObj.readLine();
-                    System.out.print("PLease enter  Key between 1 and 25");
-                    int shiftKey = Integer.parseInt(myObj.readLine());
-
-
-
-                    File file = new File(filePath);
-                    myObj reader = new myObj(new FileReader(file));
-                    String line;
-
-
-
-                    if(shiftKey>0 && shiftKey<26){
-
-
-                        while((line = reader.readLine()) != null) {
-                            Caesar msgCaesar = new Caesar(line,shiftKey);
-                            EncryptClass msgEncrypt = new EncryptClass();
-                            Decrypt msgDecrypt =new Decrypt();
-
-                            msgEncrypt.EncryptMessage(msgCaesar.getMessage(), msgCaesar.getShiftKey());
-                            msgDecrypt.decryptMessage(msgEncrypt.getEncryptedMessage(),msgCaesar.getShiftKey() );
-
-                            System.out.println("FILE ORIGINAL CONTENT: " + line);
-                            System.out.println("Encrypted Message :"+msgEncrypt.getEncryptedMessage() );
-
-                            System.out.println("Decrypted Message :"+msgDecrypt.getDecryptedMessage() );
-                        }
-                    }else{
-                        System.out.println(" Key Must be between 1 and 25");
-                    }
-
-                }else if(choice.equals("C")) {
-                    System.out.println("Successful Exit");
-                    programmeLoop = false;
-                }else {
-                    System.out.println("Invalid Choice please try again");
+                if(!newEncryption.isString()){
+                    System.out.println("Invalid Entry");
                 }
 
+                if (!newEncryption.isInRange()){
+                    System.out.println("Key Range is between 1 and 25");
+                }
 
+                String encryptedText = newEncryption.forward();
+                System.out.println(String.format("Your encrypted text is: %s", encryptedText));
 
-            }catch(IOException e){
-                e.printStackTrace();
+            }
+            else if (choice == 2){
+                System.out.println("Let's decrypt your text");
+                newScanner.nextLine();
+                System.out.println("Enter Text:");
+                String text = newScanner.nextLine();
+                System.out.println("Enter any key between 1 to 25:");
+                Integer key = Integer.parseInt(newScanner.nextLine());
+
+                Encrypt newEncryption = new Encrypt(text, key);
+
+                if(!newEncryption.isString()){
+                    System.out.println("Invalid Entry");
+                }
+
+                if (!newEncryption.isInRange()){
+                    System.out.println("Key Range is between 1 and 25");
+                }
+
+                String encryptedText = newEncryption.forward();
+
+                Decrypt newDecryption = new Decrypt(encryptedText, key);
+
+                String deryptedText = newDecryption.backward();
+                System.out.println(String.format("Your decrypted text is: %s", deryptedText));
+            }
+
+            else if (choice == 3){
+                System.out.println("Exit");
+                loopRunning=false;
+            }else if (option > 3 || option < 3){
+                System.out.println("Choose a valid choice");
+            }
+            else {
+                System.out.println("Error occured!");
             }
         }
+
+
     }
+
+
+
 }
